adaa59c81802f849194d7257bfaa3dfbf05eb202
Yuko Berger <17201835+naotomo@users.noreply.github.com>
2019-04-02 08:53:27 -0700

    setup webpack production build

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import './firebase/firebase';
-
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,50 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js', // relative path is fine!
-    output: {
-        path: path.join(__dirname, 'public'), // need absolute path!
-        filename: 'bundle.js'
-    },
-    // loader
-    module: { 
-        // specifiy how you want to load the module
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, // regex any files that end with .js
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
+module.exports = (env) => {
+    console.log('env', env);
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
-    },
-    devtool: 'cheap-module-eval-source-map', // can see which source file has a bug
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-}
+    return {
+        entry: './src/app.js', // relative path is fine!
+        output: {
+            path: path.join(__dirname, 'public'), // need absolute path!
+            filename: 'bundle.js'
+        },
+        // loader
+        module: { 
+            // specifiy how you want to load the module
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/, // regex any files that end with .js
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+    
+        },
+        plugins: [ CSSExtract ],
+        devtool: isProduction ? 'source-map':'inline-source-map', // can see which source file has a bug
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+};
